73c8433ba7027f94fd1fe65b2597055f3fea1d25
Van <vcdevx@gmail.com>
2022-06-28 21:35:57 -0400

    Completed etch-a-sketch project

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 const container = document.getElementById("container");
+const btnReset = document.getElementById("btnReset");
+const suprise = document.getElementById("suprise");
 let rows = 16;
 let cols = 16;
 
@@ -8,18 +10,39 @@ function makeRows (rows, cols) {
         for (i = 0; i < (rows * cols); i++) {
             let square = document.createElement("div");
             container.appendChild(square).className = "square";
-    square.addEventListener('mouseenter', e => {
-            square.style.backgroundColor = '#36454F';
+            square.addEventListener('mouseenter', e => {
+            if (surprise.checked) {
+                square.style.backgroundColor = randomColor();
+            } else {
+                square.style.backgroundColor = '#36454F';
+            }
         })
-        }
-        function reset() {
-            let btnReset = document.getElementById("btnReset");
-            btnReset.addEventListener('click', e => {
-                square.forEach(item => {
-                    item.style.backgroundColor = '#36454F';
-                })
-            })
-        
-        }
+        }  
 }
+
 makeRows (30, 30);
+
+btnReset.addEventListener('click', reset);
+
+function reset() { 
+    container.innerHTML = "";
+
+    let gridSizeStr = prompt("Select the amount of squares for your grid (1-100)");
+    gridSize = Number(gridSizeStr);
+    if (gridSize > 0 && gridSize <= 100) {
+        makeRows (gridSize, gridSize);
+    } else {
+        alert("Please select an integer between 1-100");
+    }
+}
+
+function randomColor() {
+    let anArray = [];
+    for (var x = 0;x < 3;x++) {
+        var randomNum = Math.floor(Math.random() * 256);
+        anArray.push(randomNum);
+    }
+    let randomRgb = 'rgb(' + anArray[0] + ',' + anArray[1] + ',' + anArray[2] + ')';
+    
+    return randomRgb;
+}
